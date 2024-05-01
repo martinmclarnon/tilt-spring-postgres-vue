cef21a92e20be476e16f2571bd0f113d4972a77a
Martin McLarnon <mfo.mclarnon@gmail.com>
2024-05-01 15:53:38 +0100

    update

--- src/api/src/main/kotlin/com/example/api/controller/BookController.kt
+++ src/api/src/main/kotlin/com/example/api/controller/BookController.kt
@@ -7,7 +7,7 @@ import com.example.api.service.BookService
 @RestController
 class BookController(val bookService: BookService) {
 
-    @CrossOrigin(origins = ["http://localhost:8081","http://canopy-lab-vm.uksouth.cloudapp.azure.com:8080"])
+    @CrossOrigin(origins = ["http://localhost:8081","http://canopy-lab-vm.uksouth.cloudapp.azure.com"])
     @GetMapping(
         value = ["/api/v1/books"],
         produces = ["application/json"]
